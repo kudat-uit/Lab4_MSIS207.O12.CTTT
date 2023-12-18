7a7e6c2654bb4e5319c3c81cd7f0ced9916f70e6
Dat Pham Thanh <75928178+kudat-uit@users.noreply.github.com>
2023-12-19 00:17:49 +0700

    upload ex1-2-3

---
+++ Ex1/src/App.js
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "assets/global.scss";
+
+import React4 from "react4";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <React3 /> */}
+
+      {/* React 4 */}
+      <React4 />
+
+    </div>
+  );
+}
+
+export default App;
